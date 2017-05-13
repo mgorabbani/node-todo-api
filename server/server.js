c2c9269d0da37824872b259057b25e4be0211d89
Golam Rabbani <knight3rrantt@gmail.com>
2017-05-13 21:18:02 +0600

    typo again! 😥

--- server/server.js
+++ server/server.js
@@ -9,7 +9,7 @@ const {User} = require('./models/user');
 
 const app = express();
 
-const port= process.env.port || 3000
+const port= process.env.PORT || 3000;
 app.use(bodyParser.json());
 
 app.post('/todos',(req,res)=>{
